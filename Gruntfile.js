6497b1e96b4937f3a4a7ada55affa899c6ad3875
David Newton <david@davidnewton.ca>
2015-03-02 13:26:49 -0500

    Basic scaffolding

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+/**
+ * grunt-respimg
+ * https://github.com/nwtn/grunt-respimg
+ *
+ * Copyright (c) 2015 David Newton
+ * Licensed under the MIT license.
+ *
+ * Automatically resizes image assets
+ *
+ * @author David Newton (http://twitter.com/newtron)
+ * @version 0.0.0
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+};
